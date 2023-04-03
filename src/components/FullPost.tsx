c8662b11f7a810abe8b280fb42d48904962a2fcf
Jonida Baci <jonida.baci@hotmail.com>
2023-04-03 21:33:04 +0200

    passed right props and refactored code

--- src/components/FullPost.tsx
+++ src/components/FullPost.tsx
@@ -1,31 +1,51 @@
-import { BiUpvote ,BiDownvote,BiHeart} from "react-icons/bi";
+import { BiUpvote, BiDownvote, BiHeart } from 'react-icons/bi'
+import { AiOutlineEye, AiOutlineComment } from 'react-icons/ai'
 
-const FullPost = ({post}) => {
+const FullPost = ({ post }) => {
+  console.log(post)
   return (
-    <div className='flex w-full h-full gap-16 justify-center text-white font-normal'>
+    <div className="flex w-full h-full gap-16 justify-center text-white font-normal">
       <div className="my-auto absolute bottom-10 flex md:flex flex-row md:flex-col items-center justify-center md:relative gap-2 px-3 py-2 md-py-5 border bg-[rgba(39,41,45,0.5)] md:bg-transparent border-gray-400 rounded-md custom-scroll-bar">
-      {post?.score!=null && <div className='flex gap-1 items-center mb-3'>
-        <BiHeart size={20} />
-        {post.score}
-        </div>}
-        {post?.ups!=null && <div className='flex gap-1 items-center mb-3'>
-        <BiUpvote size={20} />
-        {post.ups}
-        </div>}
-        {post?.downs!=null && <div className='flex gap-1 items-center mb-3'>
-        <BiDownvote size={20} />
-        {post.downs}
-        </div>}
+        {post?.score != null && (
+          <div className="flex gap-1 items-center mb-3">
+            <BiHeart size={20} />
+            {post.score}
+          </div>
+        )}
+        {post?.ups != null && (
+          <div className="flex gap-1 items-center mb-3">
+            <BiUpvote size={20} />
+            {post.ups}
+          </div>
+        )}
+
+        {post?.downs != null && (
+          <div className="flex gap-1 items-center mb-3">
+            <BiDownvote size={20} />
+            {post.downs}
+          </div>
+        )}
+        {post?.downs != null && (
+          <div className="flex gap-1 items-center mb-3">
+            <AiOutlineComment size={20} />
+            {post.comment_count}
+          </div>
+        )}
+        {post?.ups != null && (
+          <div className="flex gap-1 items-center mb-3">
+            <AiOutlineEye size={20} />
+            {post.views}
+          </div>
+        )}
       </div>
       <div className="flex-col sm:w-90 md:w-[60%] overflow-y-scroll gap-2">
-        <div className="text-xl mb-3">
-          {post?.title ?? ""}
-        </div>
-        {post?.images?.map(img=>{
+        <div className="text-xl mb-3">{post?.title ?? ''}</div>
+        {post?.images?.map((img) => {
           return (
-          <div>
-          <img src={img?.link} className='w-100 mb-2'/>
-          </div>)
+            <div>
+              <img src={img?.link} className="w-100 mb-2" />
+            </div>
+          )
         })}
       </div>
     </div>
--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -4,7 +4,7 @@ import { Card } from 'primereact/card';
 import { Button } from 'primereact/button';
 import "../styles/imageCard.scss"
 
-export const ImageCard = ({photo, description}) => {
+export const ImageCard = ({photo, description, score, upvotes, views}) => {
     const header = <img alt="Card" src={photo} />;
     return (
       <div className="p-col-4 p-md-4">
@@ -12,9 +12,9 @@ export const ImageCard = ({photo, description}) => {
           <Card header={header} className="md:w-25rem">
             <p className="m-0 text-left">{description}</p>
             <div className="card_buttons">
-              <Button label="178" icon="pi pi-arrow-up" text />
-              <Button label="252" icon="pi pi-heart-fill" text />
-              <Button label="23K " icon="pi pi-eye" text />
+              <Button label={upvotes} icon="pi pi-arrow-up" text />
+              <Button label={score} icon="pi pi-heart-fill" text />
+              <Button label={views} icon="pi pi-eye" text />
             </div>
           </Card>
         </div>
