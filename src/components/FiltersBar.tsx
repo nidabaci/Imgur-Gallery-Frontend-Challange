a845fb10fbf1aff51e53f70edb5ca218d50ae69f
Jonida Baci <jonida.baci@hotmail.com>
2023-04-04 14:19:43 +0200

    fixed filter style and added functionalities

--- src/components/FiltersBar.tsx
+++ src/components/FiltersBar.tsx
@@ -1,22 +1,62 @@
-import React from "react";
-import "../styles/filters.scss"
-import { Button } from "primereact/button";
+import React from 'react'
+import '../styles/filters.scss'
+import { Button } from 'primereact/button'
+import { useSelector, useDispatch } from '../components/hooks'
+import {
+  setFilters,
+  sectionOptions,
+  sortOptions,
+  windowOptions,
+} from '../redux/GallerySlice'
 
 export const FiltersBar = () => {
+  const dispatch = useDispatch()
+  const { section, sort, window, showViral } = useSelector(
+    (state) => state.gallery.filters
+  )
+
+  const handleFilterClick = (filterKey, filterValue) => {
+    dispatch(setFilters({ key: filterKey, value: filterValue }))
+  }
+
+  const handleViralToggle = () => {
+    dispatch(setFilters({ key: 'showViral', value: !showViral }))
+  }
   return (
     <>
       <div className="mb-10 mt-10 flex justify-center items-center filter_bar">
         <div className="container filter_button">
           <div>
-            <Button>TOP</Button>
-            <Button>HOT</Button>
-            <Button>USERS</Button>
+            <Button
+              className={section === 'top' ? 'active' : ''}
+              onClick={() => handleFilterClick('section', 'top')}
+            >
+              TOP
+            </Button>
+            <Button
+              className={section === 'hot' ? 'active' : ''}
+              onClick={() => handleFilterClick('section', 'hot')}
+            >
+              HOT
+            </Button>
+            <Button
+              className={section === 'user' ? 'active' : ''}
+              onClick={() => handleFilterClick('section', 'user')}
+            >
+              USERS
+            </Button>
           </div>
           <div className="trending_button">
-            <Button>TRENDING</Button>
+            <Button
+              className={showViral ? 'active' : ''}
+              onClick={() => handleViralToggle()}
+            >
+              TRENDING
+            </Button>
+           
           </div>
         </div>
       </div>
     </>
-  );
-};
+  )
+}
