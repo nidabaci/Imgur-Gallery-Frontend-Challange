668c2bf449b2cd22c47c16f3337abb27a249ed71
Jonida Baci <jonida.baci@hotmail.com>
2023-04-04 14:31:16 +0200

    made grid w 4 colums

--- src/App.tsx
+++ src/App.tsx
@@ -65,7 +65,7 @@ function App() {
         <Loader />
       ) : (
         <div className="container px-5 mx-auto">
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
             {postImages.map((post:any) => {
               return (
                 <div key={post?.id} className="flex flex-col" onClick={()=>{setDialogData(post)}}>
