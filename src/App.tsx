fd2552657797e3572a38f70493348ca0d9116db1
Jonida Baci <jonida.baci@hotmail.com>
2023-04-04 13:28:50 +0200

    Added paginator

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,15 @@ import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import { useEffect, useState } from 'react'
 import { useDispatch,useSelector } from './components/hooks'
-import { getGallery, incrementPage,decrementPage } from './redux/GallerySlice'
-import Filters from './components/Filters'
+import { getGallery } from './redux/GallerySlice'
 import Loader from './components/Loader'
 import MyDialog from './components/MyDialog'
 import FullPost from './components/FullPost'
 import { Header } from './components/Header'
 import { ImageCard } from './components/ImageCard';
 import { FiltersBar } from './components/FiltersBar';
+import { Pagination } from './components/Pagination';
+
 
 function App() {
   
@@ -55,11 +56,11 @@ function App() {
   return (
     <div className="">
       <Header /> 
-      <FiltersBar />
+      <FiltersBar />  
+      <Pagination/>
       <MyDialog dialogData={dialogData} setDialogData={setDialogData}>
         <FullPost post={dialogData} />
       </MyDialog>
-      <Filters />
       {pageLoading ? (
         <Loader />
       ) : (
@@ -79,10 +80,6 @@ function App() {
             })}
           </div>
          
-          <div className='flex gap-2 justify-end mt-4 pt-4 border-t-2 text-white'>
-              <button onClick={()=>{dispatch(decrementPage());window.scrollTo(0, 0)}} className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">Previous</button>
-              <button onClick={()=>{dispatch(incrementPage());window.scrollTo(0, 0)}} className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">Next</button>
-          </div>
         </div>
       )}
     </div>
