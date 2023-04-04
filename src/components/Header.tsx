52a51a17d1017e18c84cec11413225cde7948086
Jonida Baci <jonida.baci@hotmail.com>
2023-04-04 15:50:12 +0200

    styled header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,8 +7,8 @@ export const Header = () => {
     <div className="header_content">
       <h1 id="logo_text">imstone</h1>
       <div>
-        <InputText className="search_bar" placeholder=" Search for photos "  />
-        <Button className="search_button" icon="pi pi-search" />
+        <h2 className="uppercase text-center">Discover the Funniest Images on the Internet </h2>
+        <p className="text-center">Explore a vast collection of hilarious images, memes, and GIFs that will make you laugh out loud.</p>
       </div>
     </div>
   );
