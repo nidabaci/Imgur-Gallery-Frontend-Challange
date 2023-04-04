2b3049d7d268482dd343bcba8965c10d0700fa43
Jonida Baci <jonida.baci@hotmail.com>
2023-04-04 14:19:59 +0200

    disabled button when page is 1

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,25 +1,15 @@
-import React from 'react'
 import { AiOutlineArrowRight, AiOutlineArrowLeft } from 'react-icons/ai'
-import { useDispatch } from '../components/hooks'
+import { useDispatch, useSelector } from '../components/hooks'
+import { RootState } from '../redux/index'
 import { incrementPage, decrementPage } from '../redux/GallerySlice'
-import "../styles/paginator.scss"
+import '../styles/paginator.scss'
 
 export const Pagination = () => {
   const dispatch = useDispatch()
+  const page = useSelector((state: RootState) => state.gallery.filters.page)
   return (
     <>
-    
       <div className="paginator flex gap-2 justify-end mb-4 pt-4">
-        <button
-          onClick={() => {
-            dispatch(decrementPage())
-            window.scrollTo(0, 0)
-          }}
-          className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-4 px-4 border border-gray-400 rounded-full shadow"
-        >
-          <AiOutlineArrowLeft />
-        </button>
-        <div className="vertical_line"></div>
         <button
           onClick={() => {
             dispatch(incrementPage())
@@ -28,6 +18,17 @@ export const Pagination = () => {
           className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-4 px-4 border border-gray-400 rounded-full shadow"
         >
           <AiOutlineArrowRight />
+        </button>
+        <div className="vertical_line"></div>
+        <button
+          onClick={() => {
+            dispatch(decrementPage())
+            window.scrollTo(0, 0)
+          }}
+          disabled={page === 1}
+          className={`${page === 1 ? 'bg-gray-400' : 'bg-white hover:bg-gray-100'} py-4 px-4 border border-gray-400 rounded-full shadow text-gray-800 font-semibold`}
+        >
+          <AiOutlineArrowLeft />
         </button>
       </div>
     </>
